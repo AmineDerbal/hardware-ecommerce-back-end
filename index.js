bceaf7c468a10bb9927b29c68ee511a591349672
Amine derbal <aminedoran31@gmail.com>
2023-12-25 15:19:49 +0100

    Config auth route

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoute from './routes/authentification.js';
 
 const app = express();
 dotenv.config();
 app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoute);
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log('DB connection successful!'))
   .catch((err) => console.log(err));
-
-app.use(express.json());
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!');
