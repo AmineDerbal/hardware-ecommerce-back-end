ea44877da41ab4dc1db18faf42e21c7159209d5c
Amine derbal <aminedoran31@gmail.com>
2023-12-26 15:23:11 +0100

    Add User route

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRoute from './routes/authentification.js';
+import userRoute from './routes/user.js';
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 mongoose
   .connect(process.env.MONGO_URL)
