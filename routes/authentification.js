f11985dd8868fb9b27dca5b68c862dbae9b2b1d5
Amine derbal <aminedoran31@gmail.com>
2023-12-26 15:22:48 +0100

    Add login route

--- routes/authentification.js
+++ routes/authentification.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import User from '../models/User.js';
 import CryptoJS from 'crypto-js';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
@@ -18,6 +19,33 @@ router.post('/register', async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json('Wrong credentials!');
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC,
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    OriginalPassword !== req.body.password &&
+      res.status(401).json('Wrong credentials!');
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: '3d' },
+    );
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
