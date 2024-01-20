69031d0bc7b9dcc1c92cc64a2b08f6332bcc4b77
Amine derbal <aminedoran31@gmail.com>
2024-01-20 13:39:21 +0100

    Update model

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-  title: { type: String, required: true, unique: true },
-  desc: { type: String, required: true },
-  img: { type: String, required: true },
-  categories: { type: Array },
-  subCategories: { type: Array },
-  price: { type: Number, required: true },
-  inStock: { type: Boolean, default: true },
-});
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    subCategories: { type: Array },
+    price: { type: Number, required: true },
+    inStock: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
