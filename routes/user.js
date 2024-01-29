09210bc211e3754d13e974db95455f698b9013d2
Amine derbal <aminedoran31@gmail.com>
2024-01-29 15:51:03 +0100

    update get all users function

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,18 @@
 import User from '../models/User.js';
 import { Router } from 'express';
-
+import {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} from '/verifyToken.js';
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
   try {
-    const users = await User.find();
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
