c0c333f87669a3bf91e3d2a3aee3bb2cb0f35bbb
Amine derbal <aminedoran31@gmail.com>
2024-01-30 13:47:15 +0100

    Update Api Get User

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ import {
 } from '/verifyToken.js';
 const router = Router();
 
+// GET ALL USERS
 router.get('/', verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new;
   try {
@@ -14,6 +15,17 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
       ? await User.find().sort({ _id: -1 }).limit(5)
       : await User.find();
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get User
+router.get('/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
