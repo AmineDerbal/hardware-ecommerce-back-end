574d8850c1b9941093cbab3ca53571fecc6cb470
Amine derbal <aminedoran31@gmail.com>
2024-01-31 15:54:18 +0100

    Add User update route

--- routes/user.js
+++ routes/user.js
@@ -31,4 +31,27 @@ router.get('/:id', verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//UPDATE
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC,
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true },
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 export default router;
