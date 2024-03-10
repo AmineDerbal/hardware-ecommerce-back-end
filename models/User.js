c08ebb2e524597ca3ecfc573e0cf7aa0ffaad6ea
youcefRG <youyou.si@outlook.fr>
2024-03-10 15:40:10 +0100

    Update schema

--- models/User.js
+++ models/User.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
-    img: { type: String },
+    img: { type: Buffer, default: null },
+    accessToken: { type: String, default: null },
   },
   { timestamps: true },
 );
